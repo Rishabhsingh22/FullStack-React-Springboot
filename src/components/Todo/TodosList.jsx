3eaa40e15402bb807c7e708e29b748e0df59ab67
Rishabh Singh <rishabh.singh2223@gmail.com>
2025-06-03 20:14:44 +0530

    renamed file

--- src/components/Todo/TodosList.jsx
+++ src/components/Todo/TodosList.jsx
@@ -7,10 +7,10 @@ class ListTodos extends Component{
 
         this.state = {
             todos: [
-                {id:1, description: "Learn React"},
-                {id:2, description: "Become Java Expert"},
-                {id:3, description: "Learn Springboot"},
-                {id:4, description: "Learn JPA "}
+                {id:1, description: "Learn React", done: false, targetDate: new Date()},
+                {id:2, description: "Become Java Expert", done: false, targetDate: new Date()},
+                {id:3, description: "Learn Springboot", done: false, targetDate: new Date()},
+                {id:4, description: "Learn JPA ", done: false, targetDate: new Date()}
             ]
         }
     }
@@ -24,14 +24,18 @@ class ListTodos extends Component{
                         <tr>
                             <th>ID</th>
                             <th>Description</th>
+                            <th>Is Completed</th>
+                            <th>Target Date</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             this.state.todos.map(
-                                todo=> <tr key={todo.id}>
+                            todo => <tr key={todo.id}>
                             <td>{todo.id}</td>
                             <td>{todo.description}</td>
+                            <td>{todo.done.toString()}</td>
+                            <td>{todo.targetDate.toString()}</td>
                         </tr>
                             )
                         }
